adec289b8bc4e717792e92e1af3f7fb76963fb5a
ghan <hge427568@gmail.com>
2024-03-06 22:15:09 +0900

    hihihihi

---
+++ main2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //just for main branch
+}
---
+++ robot.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "robot cpp file to prac git" << endl;
+}
