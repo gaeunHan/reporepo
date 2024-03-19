1ccdd92b353520a13e49cec86d8b019629cbf877
ghan <hge427568@gmail.com>
2024-03-19 17:22:03 +0900

    first solving

--- main2.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    //just for main branch
-}
--- robot.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "robot cpp file to prac git" << endl;
-}
